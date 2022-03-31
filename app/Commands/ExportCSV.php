118d50e34c8912efa87f96bc9c1ee3598787f151
Aman Srivastava <aman.srivastava@acquia.com>
2022-04-01 02:34:00 +0530

    Code Rewrite.

--- app/Commands/ExportCSV.php
+++ app/Commands/ExportCSV.php
@@ -23,63 +23,82 @@ class ExportCSV extends Command
     protected $description = 'Export DATA in CSV format.';
 
     /**
+     * @var Illuminate\Support\Facades\Http
+     */
+    protected $client;
+
+    /**
+     * Output filename.
+     *
+     * @var string
+     */
+    protected $filename;
+
+    /**
+     * Search options.
+     *
+     * @var array
+     */
+    protected $options = [
+        'include_deleted' => "true",
+        'include_archived' => "true",
+        'limit' => 100,
+        'scroll' => "true",
+        'expand' => 'metadata',
+        'query' => 'iwi: -(isEmpty)'
+    ];
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $this->loadAssets();
-    }
-
-    public function loadAssets($scroll_id = NULL, $page = 1) {
         $headers = [
             'authorization' => 'Bearer ' . $this->argument('token')
         ];
-        $client = Http::widen()->withHeaders($headers);
-        $options = [
-            'include_deleted' => "true",
-            'include_archived' => "true",
-            'limit' => 100,
-            'scroll' => "true",
-            'expand' => 'metadata'
-        ];
+        $this->client = Http::widen()->withHeaders($headers);
+        $connect = $this->client->get('/user');
+        $this->filename = $this->option('filename') ?? 'export.csv';
+        if($connect->successful()) {
+            $this->info("Successfully connected to Widen.");
+            $this->loadAssets();
+        }
+        else {
+            $this->error("Failed to connect to Widen.");
+        }
+    }
+
+    public function loadAssets($scroll_id = NULL, $page = 1) {
+        // Load by $scroll_id if available.
         if($scroll_id) {
-            $response = $client->get('/assets/search/scroll', [
+            $response = $this->client->get('/assets/search/scroll', [
                 'scroll_id' => $scroll_id,
                 'expand' => 'metadata'
             ]);
         }
         else{
-            $response = $client->get('/assets/search', $options);
+            $response = $this->client->get('/assets/search', $this->options);
             if($response->successful()) {
                 $this->info("Total assets are " . $response->json('total_count'));
-                $filename = $this->option('filename') ?? 'export.csv';
-                Storage::disk('local')->prepend($filename, 'webdam_id,widen_id');
+                Storage::disk('local')->put($this->filename, 'webdam_id,widen_id');
             }
         }
         if($response->successful()) {
             $data = $response->json();
-            $this->task("[$page] Writing assets ... ", $this->parseAsset($data['items']));
-            if($data['scroll_id'] && !empty($data['items'])) {
+            $collection = collect($data['items']);
+            $this->task("[$page] Writing " . $collection->count() . " assets to file ... ");
+            $collection->each(function ($item) {
+                    $data = $item['metadata']['fields']['webdam_id'][0] . "," . $item['id'];
+                    Storage::disk('local')->append($this->filename, $data);
+            });
+            if($data['scroll_id'] && $collection->isNotEmpty()) {
                 $this->loadAssets($data['scroll_id'], ++$page);
             }
             else {
-                $this->task("All assets are exported.");
+                $this->info("All assets are exported successfully.");
             }
         }
-    }
-
-    public function parseAsset($assets) {
-        foreach($assets as $asset) {
-            if (!empty($asset['metadata']['fields']['webdam_id'])) {
-                $this->writeToFile($asset['metadata']['fields']['webdam_id'][0] . "," . $asset['id']);
-            }
-        }
-    }
-
-    public function writeToFile($data) {
-        $filename = $this->option('filename') ?? 'export.csv';
-        Storage::disk('local')->append($filename, $data);
     }
 }
