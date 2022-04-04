f9277460d4166a0f60489fe8a277fb9ed2fa2b70
Aman Srivastava <aman.srivastava@acquia.com>
2022-04-04 09:46:50 +0000

    Add git operatios.

--- app/Commands/Migrate.php
+++ app/Commands/Migrate.php
@@ -3,9 +3,13 @@
 namespace App\Commands;
 
 use App\Traits\Token;
+use GitWrapper\GitCommand;
+use GitWrapper\GitWorkingCopy;
 use Illuminate\Support\Facades\Storage;
 use LaravelZero\Framework\Commands\Command;
+use Nette\Utils\Strings;
 use Symfony\Component\Process\Process;
+use GitWrapper\GitWrapper;
 
 class Migrate extends Command
 {
@@ -33,18 +37,20 @@ class Migrate extends Command
     {
         $this->title('AcquiaDAM');
         $this->getToken();
-        $this->task("Check pre-requisites.", function() {
-            $this->checkCommand('acli');
-            $this->checkCommand('composer');
-            $this->checkCommand('composer1');
-            $this->checkCommand('drush');
 
-            if (Storage::missing('composer.json')) {
-                throw new \Exception("Unable to read composer.json file.");
-            }
-        });
+        $this->info("Checking available binaries.");
+        $this->checkCommand('acli');
+        $this->checkCommand('composer');
+        $this->checkCommand('composer1');
+        $this->checkCommand('git');
+        $this->checkCommand('drush');
+
+        if (Storage::missing('composer.json')) {
+            throw new \Exception("Unable to read composer.json file.");
+        }
 
         $this->task('Upgrade drupal/media_acquiadam', function() {
+            return;
             $composer = $this->choice('Composer version', ['composer', 'composer1'], '0');
             $process = Process::fromShellCommandline($composer . ' require drupal/media_acquiadam:^2 -o -n');
             $status = $process->run();
@@ -57,6 +63,7 @@ class Migrate extends Command
         });
 
         $this->task('Update database', function() {
+            return;
             $process = Process::fromShellCommandline('drush updb -y');
             $status = $process->run();
             if($status === 0) {
@@ -66,36 +73,82 @@ class Migrate extends Command
         });
 
         $this->task('Configure module.', function() {
+            return;
             Process::fromShellCommandline('drush cr')->run();
             Process::fromShellCommandline('drush config:set media_acquiadam.settings token ' . $this->token . ' -y ')->run();
+            // @TODO: Make domain value configurable, like token.
             Process::fromShellCommandline('drush config:set media_acquiadam.settings domain related.widencollective.com -y ')->run();
         });
 
+        // @TODO: Re-save media types.
         $this->task('Re-save media types.', function() {
             return false;
         });
 
         $this->task('Migrate media.', function() {
-            $token = $this->getToken();
+            return;
             $file = "export" . rand(4,6) . ".csv";
-            $this->call('export:webdam-mapping', ['--token' => $token, '-f' => $file]);
+            $this->call('export:webdam-mapping', ['-f' => $file]);
             $import = Process::fromShellCommandline('drush acquiadam:update ' . getcwd() . DIRECTORY_SEPARATOR . $file);
             $import->run();
         });
 
-        $this->task('Optional media sync.', function() {
-            return false;
+        $this->task('[Optional] media sync.', function() {
+            $flag = $this->confirm("Do you want to run media sync? (It will take time)", false);
+            if($flag == TRUE) {
+                Process::fromShellCommandline('drush acquiadam:sync --method=all')->run();
+                Process::fromShellCommandline('drush queue:run media_acquiadam_asset_refresh')->run();
+                return $flag;
+            }
+            else {
+                return $flag;
+            }
         });
 
         $this->task('Export Config.', function() {
             Process::fromShellCommandline('drush cex -y ')->run();
         });
 
-        $this->task('Interactive add to git.', function() {
+        $gitWrapper = new GitWrapper();
+        $gitWrapper->streamOutput();
+        $git = new GitWorkingCopy($gitWrapper, './');
+        $this->task('Interactive add to git.', function() use ($git) {
+            $status = $git->getStatus();
+            if(!empty($status)) {
+                $git_repo_status_arr = Strings::split($status, '#\R#');
+                foreach($git_repo_status_arr as $file) {
+                    if(!empty($file)) {
+                        $changeTypeFlag = Strings::before(ltrim($file), " ");
+                        $filename = Strings::after(ltrim($file), " ");
+                        if($changeTypeFlag == 'M') {
+                            $changeType = "modified";
+                        }
+                        elseif($changeTypeFlag == 'D') {
+                            $changeType = "deleted";
+                        }
+                        else {
+                            $changeType = "added";
+                        }
+                        $flag = $this->confirm("Do you want to add " . $changeType . $filename);
+                        if($flag) {
+                            $git->add(trim($filename));
+                        }
+                    }
+                }
+            }
             return false;
         });
 
-        $this->task('Code commit.', function() {
+        $this->task('Code commit.', function() use ($git) {
+            if ($git->hasChanges()) {
+                $git->status();
+                $flag = $this->confirm("Do you want to commit the changes ?");
+                if($flag == TRUE) {
+                    $message = $this->ask("Commit message");
+                    $git->commit($message);
+                    return true;
+                }
+            }
             return false;
         });
 
@@ -106,7 +159,7 @@ class Migrate extends Command
 
     public function checkCommand(string $command) {
         $this->task('Checking ' . $command, function() use ($command) {
-            $status = Process::fromShellCommandline('type -P ' . $command)->run();
+            $status = Process::fromShellCommandline('command -v ' . $command)->run();
             if($status === 0) {
                 return true;
             }
